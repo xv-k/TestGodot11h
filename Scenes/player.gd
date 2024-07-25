07f5aad8d092ebb864db683d8b6707d52c044990
xk <xv_k@outlook.com>
2024-07-25 20:21:17 +0200

    trying to add walk and idle animations

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
 
-@onready var animated_sprite = $AnimatedSprite2D
-const SPEED = 200.0
-var orientation = [0,0]
 
-func _physics_process(delta):
+@onready var animated_sprite = $AnimatedSprite2D
+const SPEED = 35.0
+
+func _physics_process(_delta):
 	var direction = Input.get_vector("left", "right", "up","down")
-	position += direction * SPEED * delta
+	#position += direction * SPEED * delta
+	velocity = direction * SPEED
+	print(velocity)
+	
 	set_animation_orientation(direction)
 	
 	if (Input.is_action_just_pressed("shoot")):
@@ -19,18 +22,31 @@ func _physics_process(delta):
 #orientation will be done by mouse (movement shoeld not affect animation)
 #idle should change to walking animation
 func set_animation_orientation(direction:Vector2):
-	if (direction[0] == 0 and direction[1] == 1):
-		animated_sprite.play("idle_front")
-	if (direction[0] == 0 and direction[1] == -1):
-		animated_sprite.play("idle_back")
-	if (direction[0] == 1 and direction[1] == 0):
-		animated_sprite.flip_h = false
-		animated_sprite.play("idle_side")
-	if (direction[0] == -1 and direction[1] == 0):
-		animated_sprite.flip_h = true
-		animated_sprite.play("idle_side")
-	if (direction[0] > 0 and direction[1] > 0):
-		animated_sprite.flip_h = false
-		animated_sprite.play("idle_front_angle")
-		
-	
+	if (velocity):
+		if (direction[0] == 0 and direction[1] == 1):
+			animated_sprite.play("walk_front")
+
+		if (direction[0] == 0 and direction[1] == -1):
+			animated_sprite.play("walk_back")
+		if (direction[0] == 1 and direction[1] == 0):
+			animated_sprite.flip_h = false
+			animated_sprite.play("walk_side")
+		if (direction[0] == -1 and direction[1] == 0):
+			animated_sprite.flip_h = true
+			animated_sprite.play("walk_side")
+	else:
+		if (direction[0] == 0 and direction[1] == 1):
+			animated_sprite.play("idle_front")
+		if (direction[0] == 0 and direction[1] == -1):
+			animated_sprite.play("idle_back")
+		if (direction[0] == 1 and direction[1] == 0):
+			animated_sprite.flip_h = false
+			animated_sprite.play("idle_side")
+		if (direction[0] == -1 and direction[1] == 0):
+			animated_sprite.flip_h = true
+			animated_sprite.play("idle_side")
+		#implement this
+		#if velocity.x < 0: direction = “left”
+#elif velocity.x > 0: direction = “right”
+#elif velocity.y > 0: direction = “down”
+#elif velocity.y < 0: direction = “up”
