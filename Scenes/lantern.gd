71c351666b2ae45c0d9385e15191299a459b06d8
xk <xv_k@outlook.com>
2024-08-06 21:59:24 +0200

    added different dark, flickering lights and swinging sign

--- Scenes/lantern.gd
+++ Scenes/lantern.gd
@@ -1,5 +1,6 @@
 extends StaticBody2D
 
+@export var speed = 0
 func _on_area_2d_area_entered(area):
 	print("break")
 	$PointLight2D.enabled = false
@@ -7,3 +8,8 @@ func _on_area_2d_area_entered(area):
 
 func _on_area_2d_body_entered(body):
 	$PointLight2D.enabled = true
+	
+func _process(delta):
+	#to add flickering lights
+	if Engine.get_frames_drawn() % randi_range(10,20) == 0:
+		$PointLight2D.energy = 1 - randf_range(0, 0.3)
